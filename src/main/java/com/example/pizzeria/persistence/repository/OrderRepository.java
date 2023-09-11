360ee67288fd2eb28030ab2dd0ac9c13cd71729d
pcorqui <pcorqui@gmail.com>
2023-09-10 23:54:24 -0600

    Add application-dev.properties

--- src/main/java/com/example/pizzeria/persistence/repository/OrderRepository.java
+++ src/main/java/com/example/pizzeria/persistence/repository/OrderRepository.java
@@ -1,7 +1,11 @@
 package com.example.pizzeria.persistence.repository;
 
+import com.example.pizzeria.persistence.entity.PizzaEntity;
+import com.example.pizzeria.persistence.entity.PizzaOrder;
 import org.springframework.data.repository.ListCrudRepository;
+import org.springframework.stereotype.Repository;
 
-public interface OrderRepository extends ListCrudRepository {
+@Repository
+public interface OrderRepository extends ListCrudRepository<PizzaOrder,Integer> {
 
 }
